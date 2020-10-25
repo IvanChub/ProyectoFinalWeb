95c72df0f964ad64695a5fecfa28983a688ffe07
SelvinChoc <schocc3@miumg.edu.gt>
2020-10-25 10:00:49 -0600

    Video 61 al 70

--- app/container/erp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from container.erp.views.category.views import *
-
-app_name = 'erp'
-
-urlpatterns = [
-    path('category/list/', CategoryListView.as_view(), name='category_list'),
-]
