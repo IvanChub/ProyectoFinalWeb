1ee4b557e477a4646f86e53bcc8803293d5c6846
IvanChub <ichubp@miumg.edu.gt>
2020-10-24 23:57:25 -0600

    videos del 41 al 50

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\proyecto desarrollo web\proyectoweb\env\scripts\python.exe
+#!c:\projects\proyectoweb\env\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
